73cad1c02054be8f53be75d30eb3857d9316eff4
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-02-11 08:54:24 +0000

    Added check for string division

--- script.py
+++ script.py
@@ -5,4 +5,6 @@ def add(a, b):
 def division(a, b):
     if b == 0:
         raise ValueError("Zero division is not allowed")
+    if isinstance(a, str) or isinstance(b, str):
+        raise ValueError("String division is not allowed")
     return a / b
