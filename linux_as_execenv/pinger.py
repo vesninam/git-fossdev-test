2a6dd78f2db7e81fbdcc5f80592060a4f6d37681
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-02-25 08:27:10 +0000

    Added time offset to find last date

--- linux_as_execenv/pinger.py
+++ linux_as_execenv/pinger.py
@@ -1,11 +1,18 @@
 import requests 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 url_template = "https://simurg.space/gen_file?data=obs&date={date}"
 now = datetime.now()
-date = now.strftime("%Y-%m-%d")
-url = url_template.format(date=date)
 
-response = requests.get(url=url, stream=True)
-print(f"For {date} got: ", response)
+while True:
+    date = now.strftime("%Y-%m-%d")
+    url = url_template.format(date=date)
+
+    response = requests.get(url=url, stream=True)
+    print(f"For {date} got: ", response)
+    if response.status_code == 200:
+       print(f"Last available data are for {date}")
+       break
+    else:
+       now = now - timedelta(days=1)
 
