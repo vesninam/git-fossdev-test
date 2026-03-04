5a8a0f644d4c261865cdc2dae1ea28a7dd84cfce
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-03-04 08:13:06 +0000

    Show test-not-grant-you-from-error principle

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -2,7 +2,10 @@ import sys
 sys.path.append("../src")
 #TODO make it with `pip install -e .`
 
-from math_demo import add
+from math_demo import (
+    add,
+    add_with_bug
+)
 
 # Ранее тестирование позволяет съэкономить время позднее
 # Тесты показывают наличие ошибок, а не отсутвие 
@@ -13,8 +16,16 @@ from math_demo import add
 # Тесты покрывают как успешные так и ошибочные кейсы
 
 def test_addition():
-    assert add(2, 2) == 4
+    assert add(2, 2) == 4, "Function did not return 4"
     print("Test BASIC ADDITION PASSED")
+
+def test_addition_with_bug():
+    assert add_with_bug(2, 2) == 4, "Function did not return 4"
+    assert add_with_bug(0, 0) == 0
+    print("Test BUGGED ADDITION PASSED (does it mean code ok?)")
+    #assert add_with_bug(6, 7) == 13 # will fail here
+    
 
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
