1cac3b08f302de932fd68d060957b306ce2e9770
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-03-11 08:51:37 +0000

    Fixed typo in test

--- testing_tdd/tests/test_ndfl_basic.py
+++ testing_tdd/tests/test_ndfl_basic.py
@@ -13,7 +13,7 @@ def test_ndfl_tier_1():
     assert calculate_ndfl_tax(500_000) == 65_000
 
 def test_ndfl_tier_2():
-    assert calculate_ndfl_tax(4_000_000) == 512_000
+    assert calculate_ndfl_tax(4_000_000) == 552_000
 
 def test_ndfl_tier_3():
     assert calculate_ndfl_tax(10_000_000) == 1_602_000
