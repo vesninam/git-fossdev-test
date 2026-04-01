8e62f0473fd7dcb80bd292158c69ea48c7b1e07c
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-04-01 07:43:22 +0000

    Added field to remove packaging warinings

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -6,6 +6,8 @@ from setuptools import (
 setup(
     name = "ndfl",
     version = "0.0.1",
+    long_description = "Tax calculator",
+    long_description_content_type = "text/markdown",
     package_dir = {"": "src"},
     packages = find_packages(where="src")
 )
